ef46729253362e6b9431ca17122a318ae0a4dec9
pingoor <pingoor@gmail.com>
2016-11-29 13:34:41 +0800

    fix: 修复图层刷新后section的div没有内容的bug

--- src/SectionLayer.js
+++ src/SectionLayer.js
@@ -51,7 +51,8 @@ function (declare, lang, dojoConnect, array, topic, dom, domConstruct, domStyle,
 
         _drawSection: function (section) {
             if (!this._drawn) return;
-
+            
+            section._div.innerHTML = section._content;
             var screenPt = screenUtils.toScreenPoint(this._map.extent, this._map.width, this._map.height, section._position);
             domStyle.set(section._div, {
                 "left": screenPt.x + section.offset.x + "px",
